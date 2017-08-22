3d8f0e63cc9889f2df2fb80d204f6c8b0bf461a8
AntonioGlezP <miticopineda@gmail.com>
2017-08-22 16:30:21 -0500

    1
    calcu

---
+++ Calculadora_6B/Calculadora_6B.Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora_6B.Libreria;
+
+namespace Calculadora_6B.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos misMetodos = new Metodos();
+            Console.WriteLine(misMetodos.saludar());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculadora_6B/Calculadora_6B.Libreria/Metodos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_6B.Libreria
+{
+
+    /// <summary>
+    /// metodo publico que envia un saludo
+    /// </summary>
+   public class Metodos
+    {
+       public string saludar()
+       {
+           return "Hola mundo";
+       }
+       /// <summary>
+       /// Funcion que realiza la suma de dos numeros 
+       /// </summary>
+       /// <param name="valor1">flotante</param>
+       /// <param name="valor2">flotante</param>
+       /// <returns>resultado flotante</returns>
+       public float sumar(float valor1, float valor2)
+       {
+           var resultado = valor1 + valor2;
+           return resultado;
+
+       }
+       public float restar(float valor1, float valor2)
+       {
+           var resultado = valor1 - valor2;
+           return resultado;
+
+       }
+       public float multiplicar(float valor1, float valor2)
+       {
+           var resultado = valor1 * valor2;
+           return resultado;
+
+       }
+       public float dividir(float valor1, float valor2)
+       {
+           var resultado = valor1 / valor2;
+           return resultado;
+
+       }
+    }
+}
